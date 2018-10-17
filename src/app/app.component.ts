0178418aa8ebfa93809aa7abd157c91244f512c4
Julian Hörz <>
2018-10-17 14:57:47 +0200

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import * as ol from 'ol';
+
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,31 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'my-app';
+    title = 'my-app';
+    map: ol.Map;
+    vectorLayer: ol.vectorLayer;
+
+
+    ngOnInit():void{
+        var view = new ol.View({
+            center: ol.proj.fromLonLat([9.201047,48.658488]),
+            zoom: 14
+        });
+
+        this.map = new ol.Map({
+            loadTilesWhileAnimating: true,
+            layers: [
+                new ol.layer.Tile({
+                    source: new ol.source.OSM()
+                }),
+                this.vectorLayer
+            ],
+            target: 'map',
+            controls: new ol.Collection,
+            view: view
+        });
+    }
+
+
+
 }
