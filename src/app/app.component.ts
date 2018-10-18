6d72a1e42c7ff21b6b23cd9416da9b8a41ade7f5
Julian <julian.hoerz@web.de>
2018-10-18 07:38:13 +0200

    overlay is working

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,38 +1,75 @@
-import { Component, OnInit } from '@angular/core';
-import * as ol from 'ol';
+import { Component, OnInit, ViewChild, ViewContainerRef, ViewEncapsulation } from '@angular/core';
+import Map from 'ol/Map';
+import View from 'ol/View';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
+import { Overlay ,CdkOverlayOrigin, OverlayConfig} from '@angular/cdk/overlay';
+import {TemplatePortalDirective, ComponentPortal} from '@angular/cdk/portal';
+
 
 
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
-  styleUrls: ['./app.component.css']
+  styleUrls: ['./app.component.css'],
+  encapsulation: ViewEncapsulation.None,
+  preserveWhitespaces: false,
 })
 export class AppComponent {
     title = 'my-app';
-    map: ol.Map;
-    vectorLayer: ol.vectorLayer;
+    map: Map;
+
+
+    constructor(private overlay: Overlay, private viewContainerRef: ViewContainerRef){}
+
+    // @ViewChild('myOrigin') myOrigin: CdkOverlayOrigin;
+    // @ViewChild('myOverlay') myOverlay: TemplatePortalDirective;
+    @ViewChild(CdkOverlayOrigin) _overlayOrigin: CdkOverlayOrigin;
 
 
     ngOnInit():void{
-        var view = new ol.View({
-            center: ol.proj.fromLonLat([9.201047,48.658488]),
-            zoom: 14
-        });
 
-        this.map = new ol.Map({
-            loadTilesWhileAnimating: true,
-            layers: [
-                new ol.layer.Tile({
-                    source: new ol.source.OSM()
-                }),
-                this.vectorLayer
-            ],
+        this.openSpaghettiPanel();
+
+
+        new Map({
             target: 'map',
-            controls: new ol.Collection,
-            view: view
-        });
+            layers: [
+                new TileLayer({
+                    source: new OSM()
+                })
+            ],
+            view: new View({
+              center: [0, 0],
+              zoom: 2
+            })
+          });
     }
 
 
 
+    openSpaghettiPanel(){
+        let strategy = this.overlay.position()
+        .connectedTo(
+            this._overlayOrigin.elementRef,
+            {originX: 'start', originY: 'bottom'},
+            {overlayX: 'start', overlayY: 'top'} );
+
+        let config = new OverlayConfig({positionStrategy: strategy});
+        let overlayRef = this.overlay.create(config);
+
+
+        overlayRef.attach(new ComponentPortal(SpagettiPanel, this.viewContainerRef));
+
+    }
+
 }
+
+
+@Component({
+    selector: 'spagetti-panel',
+    templateUrl: './spagetti.component.html'
+  })
+  export class SpagettiPanel {
+    value: string = 'Omega';
+  }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,30 @@
 import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
+import { OverlayModule } from '@angular/cdk/overlay';
+import { A11yModule } from '@angular/cdk/a11y';
 
-import { AppComponent } from './app.component';
+import { AppComponent,SpagettiPanel } from './app.component';
+import { CommonModule } from '@angular/common';
+import {PlatformModule} from '@angular/cdk/platform';
+import {PortalModule} from '@angular/cdk/portal';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    SpagettiPanel
   ],
   imports: [
-    BrowserModule
+    BrowserModule,
+    OverlayModule,
+    A11yModule,
+    CommonModule,
+    PlatformModule,
+    PortalModule
   ],
   providers: [],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
+  entryComponents: [
+    SpagettiPanel
+    ]
 })
 export class AppModule { }
